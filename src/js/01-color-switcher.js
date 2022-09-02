b946aba325a0fc08b9d9d24b9f943c07aeb2bbeb
Darya Shlyakhovaya <dshlyakhovaya1992@gmail.com>
2022-09-02 12:03:04 +0300

    add task 9 part 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyElement = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', changeBgColor);
+stopBtn.addEventListener('click', stopChangeColor);
+
+let intervalId = null;
+
+function changeBgColor() {
+  intervalId = setInterval(() => {
+    bodyElement.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+function stopChangeColor() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
